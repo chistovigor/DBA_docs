8ce92aa51271ab80449dcb3b63de9cc83ed679d9
Igor Chistov <chistov.i@mail366.com>
2025-12-17 21:22:30 +0200

    add block 3 documentation links

---
+++ Kubernetes/block3.sh
@@ -0,0 +1,9 @@
+https://kubernetes.io/ru/docs/setup/learning-environment/minikube/
+
+https://minikube.sigs.k8s.io/docs/handbook/deploying/
+
+https://earthly.dev/blog/managing-k8s-with-kubeadm/
+
+# useful for read !
+
+https://selectel.ru/blog/kubernetes-review/
