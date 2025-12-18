4ade250cdc76698dafa4df88733427b26cdf1a3c
Igor Chistov <chistov.i@mail366.com>
2025-12-18 12:17:05 +0200

    add block4.sh

---
+++ Kubernetes/block4.sh
@@ -0,0 +1,9 @@
+https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.30/#pod-v1-core
+
+# what is pod ?
+
+https://habr.com/ru/companies/flant/articles/427819/
+
+# useful for pods issues diagnostics !
+
+https://kubernetes.io/ru/docs/tutorials/kubernetes-basics/explore/explore-intro/
